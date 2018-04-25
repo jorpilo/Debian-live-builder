f65857dfba0930a86d2bd79e4ed9ea0658aa6933
Jorge Pinilla López <jorpilo@gmail.com>
2018-04-25 21:51:42 +0200

    Initial branch commit

---
+++ debian-live-builder-app/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'debian-live-builder'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
