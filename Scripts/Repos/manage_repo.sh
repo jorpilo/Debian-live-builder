99e7cb9054c4f8b6e2da44cade24c3b0b51a7ffd
Jorge Pinilla López <jorpilo@gmail.com>
2018-04-27 01:32:58 +0200

    hotfix

--- Scripts/Repos/manage_repo.sh
+++ Scripts/Repos/manage_repo.sh
@@ -8,7 +8,7 @@
 #Usage: manage_repo reponame (for /repos/external_repos)
 #       manage_repo reponame repourl 
 #Create a file in packages/name/name.package with the packages selected from the repo
-
+source config.conf
 
 if [ $# -eq 2 ]; then
   repo=($1 "$2")
@@ -21,16 +21,19 @@ if [ $? -ne 0 ]; then
   dialog --title "Error" --msgbox "No internet" 0 0
   exit 1
 fi
- wget -q -O /tmp/${repo[0]}.gz ${repo[1]}/dists/$Distro/main/binary-$Arquitecture/Packages.gz
+
+ wget  -O /tmp/${repo[0]}.gz ${repo[1]}/dists/$Distro/main/binary-$Arquitecture/Packages.gz
  if [ $? -ne 0 ]; then
    dialog --title "Error" --msgbox "Couldn't download packages from ${repo[1]}" 0 0
    exit 1
  fi
- zcat /tmp/${repo[0]}.gz | awk '/Package:/{print $2} /Description:/{$1 = ""; print $0}' > /tmp/${repo[0]}.raw
- sed -i "s/'//g" /tmp/${repo[0]}.raw
- sed -i 's/"//g' /tmp/${repo[0]}.raw
+ zcat /tmp/${repo[0]}.gz | awk '/Package:/{print $2}' > /tmp/${repo[0]}.txt
+ zcat /tmp/${repo[0]}.gz | awk '/Description:/{$1 = ""; print $0}' > /tmp/${repo[0]}.des.txt
+ sed -i "s/'//g" /tmp/${repo[0]}.txt
+ sed -i 's/"//g' /tmp/${repo[0]}.txt
+  sed -i "s/'//g" /tmp/${repo[0]}.des.txt
+ sed -i 's/"//g' /tmp/${repo[0]}.des.txt
 
- echo ${repo[0]}
  if [ ! -d packages/${repo[0]} ]; then
   mkdir packages/${repo[0]}
  fi
@@ -38,21 +41,17 @@ fi
   echo "" > packages/${repo[0]}/${repo[0]}.packages
   echo "Create packages file"
 fi
-  while read package; do
-      read description
-      echo \""$description"\" >> test
-      grep -q -w $package packages/${repo[0]}/${repo[0]}.packages
+  while read -r package && read -r description <&3 ; do
+      grep -w $package packages/${repo[0]}/${repo[0]}.packages
       if [ $? -eq 0 ]; then
-          echo "$package" \""$description"\" on >> /tmp/${repo[0]}.check
+          echo \""$package"\" \""$description"\" on >> /tmp/${repo[0]}.check
       else
-          echo "$package" \""$description"\" off >> /tmp/${repo[0]}.check
+          echo \""$package"\" \""$description"\" off >> /tmp/${repo[0]}.check
       fi
-  done < /tmp/${repo[0]}.raw
-  if [ ! -d packages/${repo[0]} ]; then
-    mkdir packages/${repo[0]}
-  fi
+  done </tmp/${repo[0]}.txt 3</tmp/${repo[0]}.des.txt
+
   packages=$(dialog --checklist "Choose packages:" 0 0 0 --file /tmp/${repo[0]}.check 3>&1 1>&2 2>&3 3>$-)
   if [ $? -eq 0 ]; then
     printf "%s\n" $packages  > packages/${repo[0]}/${repo[0]}.packages
   fi
-  rm /tmp/${repo[0]}*
+  rm /tmp/${repo[0]}.*
